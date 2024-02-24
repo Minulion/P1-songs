d2f0b7dc58d4aa7f404fda1865d3d7b3a22d0d5c
ANDREW KIM <akim227@wisc.edu>
2024-02-23 22:53:08 -0600

    Update file BackendDeveloperTests.java

---
+++ BackendDeveloperTests.java
@@ -0,0 +1,65 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class BackendDeveloperTests {
+
+    /**
+     * Tests whether or not readDate() throws an exception for an invalid file.
+     */
+    @Test
+    public void testReadDataException() {
+        IterableSortedCollection<SongInterface> tree = new ISCPlaceholder<>(); 
+	    BackendInterface backend = new BackendPlaceholder(tree);
+        try {
+            backend.readData("fakeName.file");
+        } catch IOException e {
+            //exception thrown for fake file
+        }
+    }
+
+    /**
+     * Tests if getRange() returns 0 songs when the input is invalid (min > max).
+     */
+    @Test
+    public void testGetRangeBadInput() {
+        IterableSortedCollection<SongInterface> tree = new ISCPlaceholder<>(); 
+	    BackendInterface backend = new BackendPlaceholder(tree);
+        Assertions.assertTrue(backend.getRange(2,1).size() == 0);
+    }
+
+    /**
+     * Tests if filterByGenre() returns 0 songs for an invalid genre.
+     */
+    @Test
+    public void testFakeGenre() {
+        IterableSortedCollection<SongInterface> tree = new ISCPlaceholder<>(); 
+	    BackendInterface backend = new BackendPlaceholder(tree);
+        Assertions.assertTrue(backend.filterByGenre("fakepoop").size() == 0);
+    }
+
+    /**
+     * Tests if fiveMostLive() throws an exception when getRange() is not yet called.
+     */
+    @Test
+    public void testGetRangeNotCalled() {
+        IterableSortedCollection<SongInterface> tree = new ISCPlaceholder<>(); 
+	    BackendInterface backend = new BackendPlaceholder(tree);
+        try {
+            backend.fiveMostLive();
+            Assertions.fail("no exception thrown");
+        } catch IllegalStateException e {
+            //exception thrown when getRange() not previously called
+        }
+    }
+
+    /**
+     * Tests if fiveMostLive() always returns no more than 5 songs.
+     */
+    @Test
+    public void testNoMoreThanFive() {
+        IterableSortedCollection<SongInterface> tree = new ISCPlaceholder<>(); 
+	    BackendInterface backend = new BackendPlaceholder(tree);
+        backend.getRange(-999,999);
+        Assertions.assertTrue(backend.fiveMostLive().size() < 6);
+    }
+}
